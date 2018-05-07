3417dd969d576ca3c2f202c089494974da4a8b5c
Michael Kropat <mail@michael.kropat.name>
2018-05-06 20:15:02 -0400

    Render websocket push updates in UI

--- WebSocketDemo/wwwroot/app.js
+++ WebSocketDemo/wwwroot/app.js
@@ -5,37 +5,61 @@ window.WebSocketDemo = window.WebSocketDemo || {};
 window.WebSocketDemo.initApp = (selectors) => {
   let elements = window.WebSocketDemo.wrapSelectors(selectors);;
   let store = new window.WebSocketDemo.Store();
+  let websocket;
 
   const initialize = () => {
     store.onUpdate(render);
 
     store.update({
       isLoading: false,
-      jobHistory: [],
-      result: null,
+      pollJobHistory: [],
+      pollResult: null,
+      pushJobHistory: [],
+      pushResult: null,
     });
 
     elements.hashButton.update(b => b.addEventListener('click', handleClick));
+
+    let antiCswshToken = parseCookie(document.cookie).antiCswshToken || '';
+    websocket = new WebSocket(getWebsocketUrl('/websocket', `?antiCswshToken=${encodeURIComponent(antiCswshToken)}`));
+    websocket.addEventListener('open', console.log('websocket opened'));
+    websocket.addEventListener('error', console.error.bind(console, 'WebSocket error'));
   };
 
   const render = () => {
-    const { isLoading, jobHistory, result } = store.state;
+    const { isLoading, pollJobHistory, pollResult, pushJobHistory, pushResult } = store.state;
 
     elements.hashButton.update(button => {
       button.disabled = isLoading ? 'disabled' : null;
     });
 
-    elements.jobHistory.update(container => {
-      container.innerText = jobHistory.join(', ');
+    elements.pollJobHistory.update(container => {
+      container.innerText = pollJobHistory.join(', ');
     });
 
-    elements.results.update(container => {
-      container.style.display = result ? '' : 'none';
+    elements.pollResult.update(container => {
+      container.style.display = pollResult ? '' : 'none';
 
-      if (result) {
-        Object.keys(result).forEach(key => {
+      if (pollResult) {
+        Object.keys(pollResult).forEach(key => {
           container.querySelectorAll('.' + key).forEach(element => {
-            element.innerText = result[key];
+            element.innerText = pollResult[key];
+          });
+        });
+      }
+    });
+
+    elements.pushJobHistory.update(container => {
+      container.innerText = pushJobHistory.join(', ');
+    });
+
+    elements.pushResult.update(container => {
+      container.style.display = pushResult ? '' : 'none';
+
+      if (pushResult) {
+        Object.keys(pushResult).forEach(key => {
+          container.querySelectorAll('.' + key).forEach(element => {
+            element.innerText = pushResult[key];
           });
         });
       }
@@ -43,32 +67,84 @@ window.WebSocketDemo.initApp = (selectors) => {
   };
 
   const handleClick = () => {
-    store.update({ isLoading: true, jobHistory: [], result: null });
-
-    fetch('/api/hash', { method: 'POST', body: elements.dataInput.value })
-      .then(pollUntilComplete)
-      .then(result => store.update({ result }))
-      .finally(() => store.update({ isLoading: false }));
-  }
-
-  const pollUntilComplete = (jobResponse) => jobResponse.json()
-    .then(job => {
-      store.update({ jobHistory: [...store.state.jobHistory, job.status] });
-
-      if (job.status !== 'complete') {
-        let selfLink = job._links.find(x => x.rel === 'self');
-        if (!selfLink) {
-          throw new Error('Expected a rel=self link');
-        }
-        return delay(1000)
-          .then(() => fetch(selfLink.href))
-          .then(pollUntilComplete);
-      }
-
-      return job.result;
+    store.update({
+      isLoading: true,
+      pollJobHistory: [],
+      pollResult: null,
+      pushJobHistory: [],
+      pushResult: null,
     });
 
+    let hashResult = fetch('/api/hash', { method: 'POST', body: elements.dataInput.value })
+      .then(response => response.json());
+
+    let polling = hashResult
+      .then(pollUntilComplete)
+      .then(result => store.update({ pollResult: result }));
+
+    let push = hashResult
+      .then(originalJob => new Promise(resolve => {
+        let eventHandler = handlePushUpdates(originalJob.id, result => resolve({ eventHandler, result }));
+        websocket.addEventListener('message', eventHandler);
+      }))
+      .then(({ eventHandler, result }) => {
+        websocket.removeEventListener('message', eventHandler);
+        store.update({ pushResult: result });
+      });
+
+    Promise.all([polling, push])
+      .finally(() => store.update({ isLoading: false }));
+  };
+
+  const pollUntilComplete = (job) => {
+    store.update({ pollJobHistory: [...store.state.pollJobHistory, job.status] });
+
+    if (job.status !== 'complete') {
+      let selfLink = job._links.find(x => x.rel === 'self');
+      if (!selfLink) {
+        throw new Error('Expected a rel=self link');
+      }
+      return delay(1000)
+        .then(() => fetch(selfLink.href))
+        .then(r => r.json())
+        .then(pollUntilComplete);
+    }
+
+    return Promise.resolve(job.result);
+  };
+
+  const handlePushUpdates = (jobId, onCompletion) => message => {
+    let job = JSON.parse(message.data);
+
+    if (job.id !== jobId)
+      return;
+
+    store.update({ pushJobHistory: [...store.state.pushJobHistory, job.status] });
+
+    if (job.status === 'complete') {
+      onCompletion(job.result);
+    }
+  };
+
   const delay = ms => new Promise(r => setTimeout(r, ms));
+
+  const getWebsocketUrl = (pathname='', search='') => {
+    let url = new URL(window.location.href);
+    url.protocol = (window.location.protocol === 'http:') ? 'ws:' : 'wss:';
+    url.pathname = pathname;
+    url.search = search;
+    return url.toString();
+  }
+
+  const parseCookie = (cookie='') => {
+    let result = {};
+    cookie.split(/;\s*/).forEach(part => {
+      let [key, ...rest] = part.split(/=/);
+      let value = rest.join('=');
+      result[decodeURIComponent(key)] = decodeURIComponent(value);
+    });
+    return result;
+  };
 
   initialize();
 };
